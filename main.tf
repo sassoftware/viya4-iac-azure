2028db54c3448f875e95a7bd3915ea237c2d75c2
Norman Johnson <norman.johnson@sas.com>
2020-11-06 10:30:33 -0500

    add partner id

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ provider "azurerm" {
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
+  partner_id      = var.partner_id
 
   features {}
 }
