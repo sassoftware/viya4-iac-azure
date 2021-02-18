72c234b7718afd4fb1896196a13d1eecabc1229d
Thomas S. Pangborn <thomas.pangborn@sas.com>
2021-02-18 18:54:57 -0500

    entrypoint script update.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # setup container user
-echo "viya4-iac-aws:*:$(id -u):$(id -g):,,,:/viya4-iac-aws:/bin/bash" >> /etc/passwd
+echo "viya4-iac-aws:*:$(id -u):$(id -g):,,,:/viya4-iac-azure:/bin/bash" >> /etc/passwd
 echo "viya4-iac-aws:*:$(id -G | cut -d' ' -f 2)" >> /etc/group
 
 exec /bin/terraform $@
