a47950833abbf7161ddcf4be208ed5e5ef26ac41
Andy Bouts <andy.bouts@sas.com>
2021-03-08 15:54:05 -0500

    Update TF Env Vrbl script

--- files/TerraformEnvVariableAssignment.sh
+++ files/TerraformEnvVariableAssignment.sh
@@ -1,6 +1,11 @@
 # usage: source <path-to-file>TerraformEnvVariableAssignment.sh
+# usage: you must comment / uncomment one of rows 3-4
+export YourSP="MTES_Customer_Advisory_SP"
+#export YourSP=$USER
 
 echo -e "You must have an active az cli login 'az login' before this script will work"
+echo -e "We will use the Service Principal for >>>>> $YourSP <<<<<"
+echo -e "Otherwise comment line 23, uncomment 24, and edit 6 to use the following"
 echo -e "\nPausing for 5 seconds; use ctrl-c to exit and login"
 sleep 5s
 
@@ -9,17 +14,27 @@ export TF_VAR_tenant_id=$(az account show --query 'tenantId' --output tsv)
 echo -e "\nTF_VAR_tenant_id:"
 echo -e $TF_VAR_tenant_id
 
+# set the subscription Name from a query; validate
+export TF_VAR_subscription_name=$(az account show --query 'name' --output tsv)
+echo -e "\nTF_VAR_subscription_name:"
+echo -e $TF_VAR_subscription_name
+
 # set the subscription ID from a query; validate
 export TF_VAR_subscription_id=$(az account show --query 'id' --output tsv)
 echo -e "\nTF_VAR_subscription_id:"
 echo -e $TF_VAR_subscription_id
 
-export TF_VAR_client_secret=$(az ad sp create-for-rbac --skip-assignment --name http://$USER --query password --output tsv)
+# obtain the client ID
+export TF_VAR_client_id=$(az ad sp show --id http://$YourSP --query appId --output tsv)
+echo -e "\nTF_VAR_client_id for this Service Principal $YourSP:"
+echo -e $TF_VAR_client_id
+
+# obtain a client secret
+export TF_VAR_client_secret=$(az ad sp create-for-rbac --skip-assignment --name http://$YourSP --query password --output tsv)
 echo -e "\nTF_VAR_client_secret:"
 echo -e $TF_VAR_client_secret
 
-export TF_VAR_client_id=$(az ad sp show --id http://$USER --query appId --output tsv)
-echo -e "\nTF_VAR_client_id:"
-echo -e $TF_VAR_client_id
-
 echo -e "\nAll done"
+echo -e "\nIf the wrong subscription was selected, then suggest:"
+echo -e "   \$az account list --output table"
+echo -e "   \$az account set --subscription <YourSubscription>"
