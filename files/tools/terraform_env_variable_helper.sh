e8304919fb6ca0e1f76d0e0f1f9f9f2cea69c294
Andy Bouts <andy.bouts@sas.com>
2021-05-12 12:13:38 -0400

    Commit to resolve JIRA IAC 248

--- files/tools/terraform_env_variable_helper.sh
+++ files/tools/terraform_env_variable_helper.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
+# `set -e` is used below to ensure that all of the assignments in the script are made / that it runs in entirety
 set -e
 
-# usage: source <path-to-file>TerraformEnvVariableAssignment.sh
-# note: select one of the following options for your Service Principal
-export YourSP="<yourSP>"
-#export YourSP=$USER
-
-echo -e "You must have an active az cli login 'az login' before this script will work"
-echo -e "We will use the Service Principal for >>>>> $YourSP <<<<<"
-echo -e "Otherwise edit the export YourSP earlier in this script "
-echo -e "\nPausing for 5 seconds; use ctrl-c to exit and login"
-sleep 5s
+echo -e "\nUsage: You must have an active az cli login 'az login' before this script will work"
+echo -e "\nUsage: Run an export referenced in line 8 or 9 before this script before continuing"
+echo "    For example: export YOURSP=your-SP-name or export YOURSP=\$USER"
+echo -e "\nUsage: source ./<path-to-file>TerraformEnvVariableAssignment.sh; e.g. source ./files/tools/terraform_env_variable_helper.sh"
+echo -e "\nUsage: The variable \$YOURSP needs to be set and currently has a value of Service Principal: >>>>> $YOURSP <<<<<"
+echo -e "\nPausing for 7 seconds so you can review; use ctrl-c to exit and login"
+sleep 7s
 
 # set the tenant ID from a query; validate
 TF_VAR_tenant_id=$(az account show --query 'tenantId' --output tsv)
@@ -31,18 +29,24 @@ echo -e "\nTF_VAR_subscription_id:"
 echo -e "$TF_VAR_subscription_id"
 
 # obtain the client ID
-TF_VAR_client_id=$(az ad sp show --id http://$YourSP --query appId --output tsv)
+TF_VAR_client_id=$(az ad sp show --id http://$YOURSP --query appId --output tsv)
 export TF_VAR_client_id
-echo -e "\nTF_VAR_client_id for this Service Principal $YourSP:"
+echo -e "\nTF_VAR_client_id for this Service Principal $YOURSP:"
 echo -e "$TF_VAR_client_id"
 
 # obtain a client secret
-TF_VAR_client_secret=$(az ad sp create-for-rbac --skip-assignment --name http://$YourSP --query password --output tsv)
+TF_VAR_client_secret=$(az ad sp create-for-rbac --skip-assignment --name http://$YOURSP --query password --output tsv)
 export TF_VAR_client_secret
 echo -e "\nTF_VAR_client_secret:"
 echo -e "$TF_VAR_client_secret"
+
+# `set +e` reverses the `set -e`
+set +e
 
 echo -e "\nAll done"
 echo -e "\nIf the wrong subscription was selected, then suggest:"
 echo -e "   \$az account list --output table"
 echo -e "   \$az account set --subscription <YourSubscription>"
+
+echo -e "\nIf the azurerm module is returning an http/401 error, then you might want to reset your SP credential:"
+echo -e "   \$az ad sp credential reset --name $YOURSP"
