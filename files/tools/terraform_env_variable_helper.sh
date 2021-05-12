d31636223889d18a37dd74770c6c4d541bdd37df
Andy Bouts <andy.bouts@sas.com>
2021-05-12 08:21:01 -0400

    Fix helper script Jira IAC-248

--- files/tools/terraform_env_variable_helper.sh
+++ files/tools/terraform_env_variable_helper.sh
@@ -1,43 +1,56 @@
 #!/usr/bin/env bash
+# `set -e` is used below to ensure that all of the assignments in the script are made / that it runs in entirety
 set -e
 
+# usage: run the export in either line 8 or 9 before this script or edit lines 8 or 9 in this script before running
 # usage: source <path-to-file>TerraformEnvVariableAssignment.sh
 # note: select one of the following options for your Service Principal
 export YourSP="<yourSP>"
 #export YourSP=$USER
 
-echo -e "You must have an active az cli login 'az login' before this script will work"
-echo -e "We will use the Service Principal for >>>>> $YourSP <<<<<"
-echo -e "Otherwise edit the export YourSP earlier in this script "
-echo -e "\nPausing for 5 seconds; use ctrl-c to exit and login"
-sleep 5s
+echo -e "Usage: You must have an active az cli login 'az login' before this script will work"
+echo -e "Usage: Run the export in either line 8 or 9 before this script or edit lines 8 or 9 in this script before running"
+echo -e "Usage: The variable \$YourSP needs to be set and currently has a value of Service Principal: >>>>> $YourSP <<<<<"
+echo -e "\nPausing for 7 seconds so you can review; use ctrl-c to exit and login"
+sleep 7s
 
 # set the tenant ID from a query; validate
-export TF_VAR_tenant_id=$(az account show --query 'tenantId' --output tsv)
+TF_VAR_tenant_id=$(az account show --query 'tenantId' --output tsv)
+export TF_VAR_tenant_id
 echo -e "\nTF_VAR_tenant_id:"
-echo -e $TF_VAR_tenant_id
+echo -e "$TF_VAR_tenant_id"
 
 # set the subscription Name from a query; validate
-export TF_VAR_subscription_name=$(az account show --query 'name' --output tsv)
+TF_VAR_subscription_name=$(az account show --query 'name' --output tsv)
+export TF_VAR_subscription_name
 echo -e "\nTF_VAR_subscription_name:"
-echo -e $TF_VAR_subscription_name
+echo -e "$TF_VAR_subscription_name"
 
 # set the subscription ID from a query; validate
-export TF_VAR_subscription_id=$(az account show --query 'id' --output tsv)
+TF_VAR_subscription_id=$(az account show --query 'id' --output tsv)
+export TF_VAR_subscription_id
 echo -e "\nTF_VAR_subscription_id:"
-echo -e $TF_VAR_subscription_id
+echo -e "$TF_VAR_subscription_id"
 
 # obtain the client ID
-export TF_VAR_client_id=$(az ad sp show --id http://$YourSP --query appId --output tsv)
+TF_VAR_client_id=$(az ad sp show --id http://$YourSP --query appId --output tsv)
+export TF_VAR_client_id
 echo -e "\nTF_VAR_client_id for this Service Principal $YourSP:"
-echo -e $TF_VAR_client_id
+echo -e "$TF_VAR_client_id"
 
 # obtain a client secret
-export TF_VAR_client_secret=$(az ad sp create-for-rbac --skip-assignment --name http://$YourSP --query password --output tsv)
+TF_VAR_client_secret=$(az ad sp create-for-rbac --skip-assignment --name http://$YourSP --query password --output tsv)
+export TF_VAR_client_secret
 echo -e "\nTF_VAR_client_secret:"
-echo -e $TF_VAR_client_secret
+echo -e "$TF_VAR_client_secret"
+
+# `set +e` reverses the `set -e`
+set +e
 
 echo -e "\nAll done"
 echo -e "\nIf the wrong subscription was selected, then suggest:"
 echo -e "   \$az account list --output table"
 echo -e "   \$az account set --subscription <YourSubscription>"
+
+echo -e "\nIf the azurerm module is returning an http/401 error, then you might want to reset your SP credential:"
+echo -e "   \$az ad sp credential reset --name $YourSP"
