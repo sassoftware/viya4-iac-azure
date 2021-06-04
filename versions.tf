08e2aa161b4a17cafc8068f6e97ca25de8b06422
Thomas S. Pangborn <thomas.pangborn@sas.com>
2021-06-04 12:24:30 -0400

    Added in  flag into netapp volume resource

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.60.0"
+      version = "2.62.0"
     }
     azureread = {
       source  = "hashicorp/azuread"
@@ -36,7 +36,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.2.0"
+      version = "2.3.1"
     }
   }
 }
