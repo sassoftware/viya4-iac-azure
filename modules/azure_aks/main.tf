3eed6b4bafd6ed153c644963928e3e3f4c23da83
Thomas S. Pangborn <thomas.pangborn@sas.com>
2021-07-13 17:09:53 -0400

    Removed extra : causing syntax issue

--- modules/azure_aks/main.tf
+++ modules/azure_aks/main.tf
@@ -1,7 +1,7 @@
 locals {
   private_cluster_enabled = var.aks_cluster_endpoint_public_access_cidrs[0] == "0.0.0.0/32" ? true : false
   private_create_uai = var.aks_uai_name == null ? true : false
-  uai_id = (local.private_cluster_enabled && local.private_create_uai) ? : azurerm_user_assigned_identity.uai.0.id : data.azurerm_user_assigned_identity.uai.0.id
+  uai_id = (local.private_cluster_enabled && local.private_create_uai) ? azurerm_user_assigned_identity.uai.0.id : data.azurerm_user_assigned_identity.uai.0.id
 }
 
 data "azurerm_user_assigned_identity" "uai" {
