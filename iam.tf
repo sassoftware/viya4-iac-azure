6b0894c3b456ea3918d7d43fcd45800104b6b79a
Matthias Ender <matthias.ender@sas.com>
2021-10-04 10:18:12 -0400

    fix role for uai for accessing byo network

--- iam.tf
+++ iam.tf
@@ -17,20 +17,20 @@ resource "azurerm_user_assigned_identity" "uai" {
   location            = var.location
 }
 
-# resource "azurerm_role_assignment" "uai_byo_rg_role" {
-#   count                = var.aks_uai_name == null ? ( var.vnet_name == null ? 0 : 1 ) : 0
-#   scope                = data.azurerm_resource_group.network_rg.0.id
-#   role_definition_name = "Contributor"
-#   principal_id         = azurerm_user_assigned_identity.uai.0.principal_id
-# }
-
 # need to be able to read and write custom route table
-resource "azurerm_role_assignment" "uai_rg_role" {
-  count                = var.aks_uai_name == null ? 1 : 0
-  scope                = local.aks_rg.id
+resource "azurerm_role_assignment" "uai_byo_rg_role" {
+  count                = var.aks_uai_name == null ? ( var.vnet_name == null ? 0 : 1 ) : 0
+  scope                = data.azurerm_resource_group.network_rg.0.id
   role_definition_name = "Contributor"
   principal_id         = azurerm_user_assigned_identity.uai.0.principal_id
 }
+
+#resource "azurerm_role_assignment" "uai_rg_role" {
+#  count                = var.aks_uai_name == null ? 1 : 0
+#  scope                = local.aks_rg.id
+#  role_definition_name = "Contributor"
+#  principal_id         = azurerm_user_assigned_identity.uai.0.principal_id
+#}
 
 
 ### TODO remove before push
