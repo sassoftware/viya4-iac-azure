35cab62e7c1760e5dbca1741fdb8cb0ee8816476
Matthias Ender <matthias.ender@sas.com>
2021-10-05 09:54:09 -0400

    fix uai-byo role condition and scope

--- iam.tf
+++ iam.tf
@@ -13,21 +13,14 @@ resource "azurerm_user_assigned_identity" "uai" {
 
 # need to be able to read and write custom route table in the BYO network
 resource "azurerm_role_assignment" "uai_byo_rg_role" {
-  count                = var.aks_uai_name == null ? ( var.vnet_name == null ? 0 : 1 ) : 0
-  scope                = data.azurerm_resource_group.network_rg.0.id
+  count                = ( var.aks_identity == "uai"
+                           ? ( var.aks_uai_name == null
+                               ? ( var.vnet_name == null ? 0 : 1 )
+                               : 0
+                             )
+                           : 0
+                         )
+  scope                = local.network_rg.id
   role_definition_name = "Contributor"
   principal_id         = azurerm_user_assigned_identity.uai.0.principal_id
-}
-
-#resource "azurerm_role_assignment" "uai_rg_role" {
-#  count                = var.aks_uai_name == null ? 1 : 0
-#  scope                = local.aks_rg.id
-#  role_definition_name = "Contributor"
-#  principal_id         = azurerm_user_assigned_identity.uai.0.principal_id
-#}
-
-
-### TODO remove before push
-output "aks_uai_id" {
-  value = local.aks_uai_id
 }
