df23f6d9d58232ab01f3dc9c02b67e3e7242a215
Ritika Patil <Ritika.Patil@sas.com>
2022-08-24 12:52:59 -0400

    feat: (IAC-583): Removed data source for the secret, code will use the new resource kubernetes_secret

--- modules/kubeconfig/main.tf
+++ modules/kubeconfig/main.tf
@@ -19,6 +19,7 @@ data "template_file" "kubeconfig_provider" {
   }
 }
 
+# Service Account based kube config data/template/resources
 # 1.24 change: Create service account secret
 resource "kubernetes_secret" "sa_secret" {
   count = var.create_static_kubeconfig ? 1 : 0
@@ -33,15 +34,6 @@ resource "kubernetes_secret" "sa_secret" {
   type = "kubernetes.io/service-account-token"
 }
 
-# Service Account based kube config data/template/resources
-data "kubernetes_secret" "sa_secret" {
-  count = var.create_static_kubeconfig ? 1 : 0
-  metadata {
-    name = "${kubernetes_secret.sa_secret[0].metadata.0.name}"
-    namespace = var.namespace
-  }
-}
-
 data "template_file" "kubeconfig_sa" {
   count = var.create_static_kubeconfig ? 1 : 0
   template = file("${path.module}/templates/kubeconfig-sa.tmpl")
@@ -50,8 +42,8 @@ data "template_file" "kubeconfig_sa" {
     cluster_name = var.cluster_name
     endpoint     = var.endpoint
     name         = local.service_account_name
-    ca_crt       = base64encode(lookup(data.kubernetes_secret.sa_secret.0.data,"ca.crt", ""))
-    token        = lookup(data.kubernetes_secret.sa_secret.0.data,"token", "")
+    ca_crt       = base64encode(lookup(kubernetes_secret.sa_secret.0.data,"ca.crt", ""))
+    token        = lookup(kubernetes_secret.sa_secret.0.data,"token", "")
     namespace    = var.namespace
   }
 }
