730c0479b3f5cf53f943d9d077fc17d13668656e
Ritika Patil <Ritika.Patil@sas.com>
2022-09-07 13:30:23 -0400

    feat: (IAC-403): Update AzureRM provider version to latest

--- versions.tf
+++ versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.20.0"
+      version = "3.21.1"
     }
     azuread = {
       source  = "hashicorp/azuread"
