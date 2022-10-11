67c03e54b4bbb505c6d10cf45592492a5d888d8c
Ritika Patil <Ritika.Patil@sas.com>
2022-10-11 14:29:15 -0400

    feat: (IAC-403): Updating the azurerm version to latest v3.26.0

--- versions.tf
+++ versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.21.1"
+      version = "3.26.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
