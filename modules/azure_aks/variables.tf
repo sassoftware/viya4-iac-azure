7ee5f27296ae6b9c4e1125db5c74a43eea52f61a
Ritika Patil <Ritika.Patil@sas.com>
2023-02-23 14:28:47 -0500

    feat: (IAC-900): add K8s 1.25 support, set kubectl default to 1.24

--- modules/azure_aks/variables.tf
+++ modules/azure_aks/variables.tf
@@ -67,7 +67,7 @@ variable "aks_cluster_max_pods" {
 
 variable kubernetes_version {
   description = "The AKS cluster K8s version"
-  default     = "1.23.12"
+  default     = "1.24"
 }
 variable "aks_cluster_endpoint_public_access_cidrs" {
   description = "Kubernetes cluster access IP ranges"
