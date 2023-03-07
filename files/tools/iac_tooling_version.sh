6c6461a309711e3d37c01dfdd90bf734dd6caad0
Ritika Patil <Ritika.Patil@sas.com>
2023-03-07 10:24:02 -0500

    feat: (IAC-934): Added license information to copyright block

--- files/tools/iac_tooling_version.sh
+++ files/tools/iac_tooling_version.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Copyright © 2020-2023, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 
 # We need to return an error if things don't work
 set -e
