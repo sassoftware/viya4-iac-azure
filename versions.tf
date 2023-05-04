86bbac5a2cfff548a1fc011b708d5c91dbb34ba2
Ritika Patil <Ritika.Patil@sas.com>
2023-05-04 15:29:27 -0400

    feat: (IAC-955) Added support for Standard value for aks_cluster_sku_tier

--- versions.tf
+++ versions.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.26.0"
+      version = "3.54.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
