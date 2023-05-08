e886eda3a85f26ce82617836acf1a4932db33f59
Ritika Patil <Ritika.Patil@sas.com>
2023-05-08 12:05:45 -0400

    updated versions

--- versions.tf
+++ versions.tf
@@ -3,12 +3,12 @@
 
 terraform {
 
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.4.5"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.26.0"
+      version = "3.54.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
