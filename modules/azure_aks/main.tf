d52b6d7795ebaf904c3b794a7402f133d2684772
Ritika Patil <Ritika.Patil@sas.com>
2023-10-10 11:45:20 -0500

    feat: (IAC-950) Update the dns_prefix logic

--- modules/azure_aks/main.tf
+++ modules/azure_aks/main.tf
@@ -6,8 +6,9 @@ resource "azurerm_kubernetes_cluster" "aks" {
   name                               = var.aks_cluster_name
   location                           = var.aks_cluster_location
   resource_group_name                = var.aks_cluster_rg
-  dns_prefix                         = var.aks_private_cluster == false ? var.aks_cluster_dns_prefix : null
-  dns_prefix_private_cluster         = var.aks_private_cluster ? var.aks_cluster_dns_prefix : null
+  dns_prefix                         = var.aks_private_cluster == false || var.aks_cluster_private_dns_zone_id == "" ? var.aks_cluster_dns_prefix : null
+  dns_prefix_private_cluster         = var.aks_private_cluster && var.aks_cluster_private_dns_zone_id != "" ? var.aks_cluster_dns_prefix : null
+
   sku_tier                           = var.aks_cluster_sku_tier
   role_based_access_control_enabled  = true
   http_application_routing_enabled   = false
