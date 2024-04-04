60051e506385d33d5607960d185868c20441dcb7
Ritika Patil <Ritika.Patil@sas.com>
2024-04-04 16:24:43 -0500

    docs: Updated copyright to 2024

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# Copyright © 2020-2023, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
+# Copyright © 2020-2024, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 ## Azure-AKS
