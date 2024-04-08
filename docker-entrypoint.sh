ca4ba01a15d70f3e14fffdac8cfe65be97b5309c
Ritika Patil <94649368+riragh@users.noreply.github.com>
2024-04-08 08:29:18 -0500

    docs: Updated copyright to 2024 (#375)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright © 2020-2023, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
+# Copyright © 2020-2024, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 set -e
--- files/tools/terraform_env_variable_helper.sh
+++ files/tools/terraform_env_variable_helper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright © 2020-2023, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
+# Copyright © 2020-2024, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 echo -e "\nUsage: You must have an active az cli login 'az login' before this script will work"
