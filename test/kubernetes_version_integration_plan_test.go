b0ebb103bcb8f57f19c54286a4956b13fd69c19a
chjmil <christopher.miller@sas.com>
2025-01-24 17:40:28 -0500

    feat: (PSKD-1145) Add initial plan tests
    Signed-off-by: chjmil <christopher.miller@sas.com>

---
+++ test/kubernetes_version_integration_plan_test.go
@@ -0,0 +1,61 @@
+//go:build integration_plan_tests
+
+package test
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestKubernetesVersionIntegrationPlan(t *testing.T) {
+	t.Parallel()
+
+	uniquePrefix := strings.ToLower(random.UniqueId())
+	p := "../examples/sample-input-minimal.tfvars"
+
+	var variables map[string]interface{}
+	terraform.GetAllVariablesFromVarFile(t, p, &variables)
+
+	//  add the required variables
+	variables["kubernetes_version"] = "1.30"
+	variables["prefix"] = "terratest-" + uniquePrefix
+	variables["location"] = "eastus2"
+	variables["default_public_access_cidrs"] = strings.Split(os.Getenv("TF_VAR_public_cidrs"), ",")
+
+	// Create a temporary file in the default temp directory
+	planFileName := "testplan-" + uniquePrefix + ".tfplan"
+	planFilePath := filepath.Join("/tmp/", planFileName)
+	defer os.Remove(planFilePath) // Ensure file is removed on exit
+	os.Create(planFilePath)
+
+	// Copy the terraform folder to a temp folder
+	tempTestFolder := test_structure.CopyTerraformFolderToTemp(t, "../", "")
+
+	// Configure Terraform setting up a path to Terraform code.
+	terraformOptions := &terraform.Options{
+		// The path to where our Terraform code is located.
+		TerraformDir: tempTestFolder,
+
+		// Variables to pass to our Terraform code using -var options.
+		Vars: variables,
+
+		// Configure a plan file path so we can introspect the plan and make assertions about it.
+		PlanFilePath: planFilePath,
+	}
+
+	plan := terraform.InitAndPlanAndShowWithStruct(t, terraformOptions)
+	K8s_resource := plan.ResourcePlannedValuesMap["module.aks.azurerm_kubernetes_cluster.aks"]
+	K8s_version := K8s_resource.AttributeValues["kubernetes_version"]
+	fmt.Println("--------------------------------")
+	fmt.Printf("Kubernetes version: %s \n", K8s_version)
+	fmt.Println("--------------------------------")
+	assert.Equal(t, K8s_version, variables["kubernetes_version"], "The two versions should be the same.")
+}
