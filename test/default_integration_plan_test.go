e4671ea89aaef8a47a484110fd03347364b0460f
chjmil <christopher.miller@sas.com>
2025-01-27 19:14:35 -0500

    more work on default tests
    Signed-off-by: chjmil <christopher.miller@sas.com>

--- test/default_integration_plan_test.go
+++ test/default_integration_plan_test.go
@@ -66,85 +66,54 @@ func TestGeneral(t *testing.T) {
 
 	// Get resources from plan
 	cluster := plan.ResourcePlannedValuesMap["module.aks.azurerm_kubernetes_cluster.aks"]
-	pool := plan.ResourcePlannedValuesMap["module.netapp[0].azurerm_netapp_pool.anf"]
-	volume := plan.ResourcePlannedValuesMap["module.netapp[0].azurerm_netapp_volume.anf"]
+	// pool := plan.ResourcePlannedValuesMap["module.netapp[0].azurerm_netapp_pool.anf"]
+	// volume := plan.ResourcePlannedValuesMap["module.netapp[0].azurerm_netapp_volume.anf"]
 
-	// 1. Main Jump VM Resource
-	jumpVM := plan.ResourcePlannedValuesMap["module.jump[0].azurerm_linux_virtual_machine.vm"]
-	assert.NotNil(t, jumpVM, "Jump VM should be created")
-	assert.Equal(t, jumpVM.AttributeValues["admin_username"], "jumpuser", "Unexpected jump VM admin username")
-	assert.Equal(t, jumpVM.AttributeValues["size"], "Standard_B2s", "Unexpected jump VM machine type")
+	// partner_id - Not present in tfplan
 
-	// 2. Public IP Resource
-	jumpPublicIP := plan.ResourcePlannedValuesMap["module.jump[0].azurerm_public_ip.vm_ip[0]"]
-	assert.NotNil(t, jumpPublicIP, "Jump VM public IP should exist")
-	assert.Equal(t, jumpPublicIP.AttributeValues["allocation_method"], "Static", "Jump VM should use static IP")
+	//create_static_kubeconfig - Not present in tfplan
 
-	// 3. Network Interface
-	//jumpNIC := plan.ResourcePlannedValuesMap["module.jump[0].azurerm_network_interface.nic"]
-	//assert.NotNil(t, jumpNIC, "Jump VM network interface should exist")
-
-	// 4. Network Security Group
-	//jumpNSG := plan.ResourcePlannedValuesMap["module.jump[0].azurerm_network_security_group.nsg"]
-	//assert.NotNil(t, jumpNSG, "Jump VM network security group should exist")
-
-	// 5. NSG Association
-	//jumpNSGAssoc := plan.ResourcePlannedValuesMap["module.jump[0].azurerm_network_interface_security_group_association.nsg_association"]
-	//assert.NotNil(t, jumpNSGAssoc, "Jump VM NSG association should exist")
-
-	// Save plan to JSON for debugging
-	jsonBytes, err := json.MarshalIndent(plan, "", "  ")
-
-	if err != nil {
-		t.Errorf("Failed to marshal plan to JSON: %v", err)
-	} else {
-
-		outputPath := filepath.Join("test_output", "terraform_plan.json")
-		os.MkdirAll(filepath.Dir(outputPath), 0755)
-		if err := os.WriteFile(outputPath, jsonBytes, 0644); err != nil {
-			t.Errorf("Failed to write JSON file: %v", err)
-		} else {
-			t.Logf("Full plan saved to: %s", outputPath)
-		}
-	}
-
-	// partner_id
-	// create_static_kubeconfig
-
-	// kubernetes_version - AKS cluster Kubernetes version
+	// kubernetes_version
 	k8sVersion := cluster.AttributeValues["kubernetes_version"]
-	assert.Equal(t, k8sVersion, "1.29", "Unexpected Kubernetes version")
+	assert.Equal(t, k8sVersion, "1.30", "Unexpected Kubernetes version")
 
 	// create_jump_vm
+	// Verify that the jump vm has been created
+	jumpVM := plan.ResourcePlannedValuesMap["module.jump[0].azurerm_linux_virtual_machine.vm"]
+	assert.NotNil(t, jumpVM, "Jump VM should be created")
 
 	// create_jump_public_ip
+	jumpPublicIP := plan.ResourcePlannedValuesMap["module.jump[0].azurerm_public_ip.vm_ip[0]"]
+	assert.NotNil(t, jumpPublicIP, "Jump VM public IP should exist")
 
 	// enable_jump_public_static_ip
+	assert.Equal(t, jumpPublicIP.AttributeValues["allocation_method"], "Static", "Jump VM should use static IP")
 
 	// jump_vm_admin
+	assert.Equal(t, jumpVM.AttributeValues["admin_username"], "jumpuser", "Unexpected jump VM admin username")
 
 	// jump_vm_machine_type
+	assert.Equal(t, jumpVM.AttributeValues["size"], "Standard_B2s", "Unexpected jump VM machine type")
 
-	// jump_rwx_filestore_path
+	// jump_rwx_filestore_path - in the output but not the tfplan?
 
-	// tags
+	// tags - defaults to empty so there is nothing to test. If we wanted to test it, this is how we would
+	// aksTags := cluster.AttributeValues["tags"]
+	// assert.Equal(t, aksTags, map[string]interface{}(map[string]interface{}{"test": "test"}), "Unexpected AKS Tags")
 
-	// aks_identity - UserAssignedIdentity or Service Principal for AKS
-	//identity := cluster.AttributeValues["identity"].([]interface{})[0].(map[string]interface{})
-	//assert.Equal(t, identity["type"], "UserAssigned", "Unexpected AKS identity type")
+	// aks_identity
+	userAssignedIdentity := plan.ResourcePlannedValuesMap["azurerm_user_assigned_identity.uai[0]"]
+	assert.NotNil(t, userAssignedIdentity, "The User Identity should exist.")
 
 	// ssh_public_key
+	// TODO figure this out
+	assert.NotNil(t, cluster.AttributeValues["linux_profile[0]"], "SSH Key should exist")
 
-	// cluster_api_mode - Public or private IP for cluster api
-	apiServerAccess := cluster.AttributeValues["api_server_access_profile"].([]interface{})[0].(map[string]interface{})
-	assert.Equal(t, apiServerAccess["enable_private_cluster"], false, "Unexpected cluster API mode")
+	// cluster_api_mode
 
-	// aks_cluster_private_dns_zone_id - DNS zone resource ID for private cluster
-	assert.Equal(t, apiServerAccess["private_dns_zone_id"], "", "Unexpected private DNS zone ID")
+	// aks_cluster_private_dns_zone_id
 
-	// aks_cluster_sku_tier - SKU Tier for Kubernetes Cluster
-	skuTier := cluster.AttributeValues["sku_tier"]
-	assert.Equal(t, skuTier, "Free", "Unexpected AKS SKU tier")
+	// aks_cluster_sku_tier
 
 	// cluster_support_tier
 
