eff2dfdeaf141f41eb258044a0077db775e4844f
Phkuma <38486220+PhanikumarThodupunoori@users.noreply.github.com>
2025-02-07 14:19:41 -0600

    Update nodepool_test.go

--- test/nodepool_test.go
+++ test/nodepool_test.go
@@ -1,39 +1,93 @@
+// go:build integration_plan_unit_tests
+
 package test
 
 import (
-"testing"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
 
-"github.com/gruntwork-io/terratest/modules/terraform"
-"github.com/stretchr/testify/assert"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestNodePoolsVariables(t *testing.T) {
-t.Parallel()
+// Test the default variables when using the sample-input-defaults.tfvars file.
+// Verify that the tfplan is using the default variables from the CONFIG-VARS
+func TestNodeVMAdmin(t *testing.T) {
+	t.Parallel()
 
-// Define Terraform options
-terraformOptions := &terraform.Options{
-// Path to the directory containing Terraform code
-TerraformDir: "<Add your terraform directory path>", 
-}
+	uniquePrefix := strings.ToLower(random.UniqueId())
+	p := "examples/sample-input-defaults.tfvars"
 
-// Run terraform init and terraform plan
-terraform.InitAndPlan(t, terraformOptions) 
+	var variables map[string]interface{}
+	terraform.GetAllVariablesFromVarFile(t, p, &variables)
 
-// Fetch Terraform outputs
-nodeVmAdmin := terraform.Output(t, terraformOptions, "node_vm_admin")
-nodePoolVmType := terraform.Output(t, terraformOptions, "default_nodepool_vm_type")
-nodePoolDiskSize := terraform.Output(t, terraformOptions, "default_nodepool_os_disk_size")
-nodePoolMaxPods := terraform.Output(t, terraformOptions, "default_nodepool_max_pods")
-nodePoolMinNodes := terraform.Output(t, terraformOptions, "default_nodepool_min_nodes")
-nodePoolMaxNodes := terraform.Output(t, terraformOptions, "default_nodepool_max_nodes")
-nodePoolAvailabilityZones := terraform.OutputList(t, terraformOptions, "default_nodepool_availability_zones")
+	//  add the required variables
+	variables["prefix"] = "terratest-" + uniquePrefix
+	variables["location"] = "eastus2"
+	variables["default_public_access_cidrs"] = strings.Split(os.Getenv("TF_VAR_public_cidrs"), ",")
 
-// Assertions to validate Terraform outputs
-assert.Equal(t, "azureuser", nodeVmAdmin, "node_vm_admin should be 'azureuser'")
-assert.Equal(t, "Standard_E8s_v5", nodePoolVmType, "default_nodepool_vm_type should be 'Standard_E8s_v5'")
-assert.Equal(t, "128", nodePoolDiskSize, "default_nodepool_os_disk_size should be '128'")
-assert.Equal(t, "110", nodePoolMaxPods, "default_nodepool_max_pods should be '110'")
-assert.Equal(t, "1", nodePoolMinNodes, "default_nodepool_min_nodes should be '1'")
-assert.Equal(t, "5", nodePoolMaxNodes, "default_nodepool_max_nodes should be '5'")
-assert.Equal(t, []string{"1"}, nodePoolAvailabilityZones, "default_nodepool_availability_zones should be ['1']")
+	// Create a temporary file in the default temp directory
+	planFileName := "testplan-" + uniquePrefix + ".tfplan"
+	planFilePath := filepath.Join("/tmp/", planFileName)
+	defer os.Remove(planFilePath) // Ensure file is removed on exit
+	os.Create(planFilePath)
+
+	// Configure Terraform setting up a path to Terraform code.
+	terraformOptions := &terraform.Options{
+		// The path to where our Terraform code is located.
+		TerraformDir: ".",
+
+		// Variables to pass to our Terraform code using -var options.
+		Vars: variables,
+
+		// Configure a plan file path so we can introspect the plan and make assertions about it.
+		PlanFilePath: planFilePath,
+
+		// Remove color codes to clean up output
+		NoColor: false,
+	}
+
+	plan := terraform.InitAndPlanAndShowWithStruct(t, terraformOptions)
+	cluster := plan.ResourcePlannedValuesMap["module.aks.azurerm_kubernetes_cluster.aks"]
+
+	// node_vm_admin
+	expectedNodeVMAdmin := "azureuser"
+	nodeVMAdmin := cluster.AttributeValues["linux_profile"]
+	actualNodeVMAdmin := nodeVMAdmin.([]interface{})[0].(map[string]interface{})["admin_username"]
+	assert.Equal(t, expectedNodeVMAdmin, actualNodeVMAdmin, "Unexpected Node VM Admin User")
+
+	//default_nodepool_vm_type
+	expectedNodepoolVMType := "Standard_E8s_v5"
+	nodePool := cluster.AttributeValues["default_node_pool"]
+	actualNodepoolVMType := nodePool.([]interface{})[0].(map[string]interface{})["vm_size"]
+	assert.Equal(t, expectedNodepoolVMType, actualNodepoolVMType, "Unexpected Default Node Pool VM Type")
+
+	//default_nodepool_os_disk_size
+	expectedNodepoolOSDiskSize := float64(128)
+	actualNodepoolOSDiskSize := nodePool.([]interface{})[0].(map[string]interface{})["os_disk_size_gb"]
+	assert.Equal(t, expectedNodepoolOSDiskSize, actualNodepoolOSDiskSize, "Unexpected Default Node Pool OS Disk Size")
+
+	//default_nodepool_max_pods
+	expectedNodepoolMaxPods := float64(110)
+	actualNodepoolMaxPods := nodePool.([]interface{})[0].(map[string]interface{})["max_pods"]
+	assert.Equal(t, expectedNodepoolMaxPods, actualNodepoolMaxPods, "Unexpected Default Node Pool Max Pods")
+
+	//default_nodepool_min_nodes
+	expectedNodepoolMinNodes := float64(1)
+	actualNodepoolMinNodes := nodePool.([]interface{})[0].(map[string]interface{})["min_count"]
+	assert.Equal(t, expectedNodepoolMinNodes, actualNodepoolMinNodes, "Unexpected Default Node Pool Min Nodes")
+
+	//default_nodepool_max_nodes
+	expectedNodepoolMaxNodes := float64(5)
+	actualNodepoolMaxNodes := nodePool.([]interface{})[0].(map[string]interface{})["max_count"]
+	assert.Equal(t, expectedNodepoolMaxNodes, actualNodepoolMaxNodes, "Unexpected Default Node Pool Max Nodes")
+
+	//default_nodepool_availability_zones
+	expectedNodepoolAvailability := []interface{}{"1"}
+	actualNodepoolAvailability := nodePool.([]interface{})[0].(map[string]interface{})["zones"]
+	assert.Equal(t, expectedNodepoolAvailability, actualNodepoolAvailability, "Unexpected Default Node Pool Zones")
+
 }
