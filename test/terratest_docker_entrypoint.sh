62197a78e02d815fb4208e4089a6c303f95fc6f4
chjmil <christopher.miller@sas.com>
2025-02-17 15:26:39 -0500

    feat: change terratest dockerfile implementation
    Signed-off-by: chjmil <christopher.miller@sas.com>

---
+++ test/terratest_docker_entrypoint.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Copyright © 2020-2025, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+
+# setup container user
+echo "viya4-iac-azure:*:$(id -u):$(id -g):,,,:/viya4-iac-azure:/bin/bash" >> /etc/passwd
+echo "viya4-iac-azure:*:$(id -G | cut -d' ' -f 2)" >> /etc/group
+
+. /.azure_docker_creds.env
+export TF_VAR_client_id=$TF_VAR_client_id
+export TF_VAR_client_secret=$TF_VAR_client_secret
+export TF_VAR_tenant_id=$TF_VAR_tenant_id
+export TF_VAR_subscription_id=$TF_VAR_subscription_id
+
+# package - default to .
+# test name - default to *
+# build tags - default to integration_plan_unit_tests (name pending)
+exec go test -v . -run TestDefaults -tags integration_plan_unit_tests
+
+
+
