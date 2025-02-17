de7eecfe19c71d0fb616064ca43c407e42473aeb
chjmil <christopher.miller@sas.com>
2025-02-17 15:41:36 -0500

    fix: update workflow
    Signed-off-by: chjmil <christopher.miller@sas.com>

--- test/terratest_docker_entrypoint.sh
+++ test/terratest_docker_entrypoint.sh
@@ -9,7 +9,10 @@ set -e
 echo "viya4-iac-azure:*:$(id -u):$(id -g):,,,:/viya4-iac-azure:/bin/bash" >> /etc/passwd
 echo "viya4-iac-azure:*:$(id -G | cut -d' ' -f 2)" >> /etc/group
 
-. /.azure_docker_creds.env
+if [ -f "/.azure_docker_creds.env" ]; then
+    . /.azure_docker_creds.env
+fi
+
 export TF_VAR_client_id=$TF_VAR_client_id
 export TF_VAR_client_secret=$TF_VAR_client_secret
 export TF_VAR_tenant_id=$TF_VAR_tenant_id
@@ -19,6 +22,3 @@ export TF_VAR_subscription_id=$TF_VAR_subscription_id
 # test name - default to *
 # build tags - default to integration_plan_unit_tests (name pending)
 exec go test -v . -run TestDefaults -tags integration_plan_unit_tests
-
-
-
