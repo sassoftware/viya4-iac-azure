30baa4491fe1f7650ae10618fbd95cc2b7a250a3
chjmil <christopher.miller@sas.com>
2025-03-05 16:21:44 -0500

    chore: more version bumps
    Signed-off-by: chjmil <christopher.miller@sas.com>

--- versions.tf
+++ versions.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.3.0"
+      version = "4.21.1"
     }
     azuread = {
       source  = "hashicorp/azuread"
