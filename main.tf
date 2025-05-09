50bb0c1cf1b970f7154cef5709f22f92b7ff2f15
Thomas M <frozentank@gmail.com>
2025-05-09 17:15:29 -0400

    fix: Adding permission management for BYO networks #450 (#477)
    * Adding permission management for BYO networks #450
    
    Signed-off-by: frozentank <frozentank@gmail.com>
    
    * Reversing check that looks for when BYO networks are being used.
    
    Signed-off-by: frozentank <frozentank@gmail.com>
    
    ---------
    
    Signed-off-by: frozentank <frozentank@gmail.com>
    Co-authored-by: Chris Miller <53184971+saschjmil@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -87,6 +87,9 @@ module "vnet" {
   resource_group_name = local.network_rg.name
   location            = var.location
   subnets             = local.subnets
+  roles               = var.msi_network_roles
+  aks_uai_principal_id = local.aks_uai_principal_id
+  add_uai_permissions = (var.aks_uai_name == null)
   existing_subnets    = var.subnet_names
   address_space       = [var.vnet_address_space]
   tags                = var.tags
