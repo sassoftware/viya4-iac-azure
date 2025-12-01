d9bf9306e268e23b383c576f1b0bb89bec21aa92
Lohit Dave <111415369+lohitdave@users.noreply.github.com>
2025-12-01 17:57:08 +0530

    adding K8s version 1.34 Support

--- variables.tf
+++ variables.tf
@@ -170,7 +170,7 @@ variable "default_nodepool_vm_type" {
 variable "kubernetes_version" {
   description = "The AKS cluster K8s version"
   type        = string
-  default     = "1.32"
+  default     = "1.33"
 }
 
 variable "default_nodepool_max_nodes" {
