453098946ffaf32d8412e1bbd746e4ba0a9c5159
Carus Kyle <caruskyle@gmail.com>
2026-03-13 16:07:33 +0000

    docs: update AKS network plugin/policy documentation to reflect deprecations.
    Signed-off-by: Carus Kyle <caruskyle@gmail.com>

--- variables.tf
+++ variables.tf
@@ -225,7 +225,7 @@ variable "aks_azure_policy_enabled" {
 
 # AKS advanced network config
 variable "aks_network_plugin" {
-  description = "Network plugin to use for networking. Currently supported values are azure and kubenet. Changing this forces a new resource to be created."
+  description = "Network plugin to use for networking. Currently supported values are azure and kubenet (deprecated). Changing this forces a new resource to be created."
   type        = string
   default     = "kubenet"
 
@@ -265,7 +265,7 @@ variable "aks_dns_service_ip" {
 }
 
 variable "aks_pod_cidr" {
-  description = "The CIDR to use for pod IP addresses. This field can only be set when network_plugin is set to kubenet. Changing this forces a new resource to be created."
+  description = "The CIDR to use for pod IP addresses. This field can only be set when network_plugin is set to kubenet (deprecated). Changing this forces a new resource to be created."
   type        = string
   default     = "10.244.0.0/16"
 
