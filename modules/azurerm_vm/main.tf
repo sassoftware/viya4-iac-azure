4a415cc920a0cd940b5d1758c2acfafe93710718
abhishek kumar <abhishek.kumar@sas.com>
2026-09-17 12:32:22 +0530

    fix: set primary flag for IPv4 NIC config when IPv6 is enabled

--- modules/azurerm_vm/main.tf
+++ modules/azurerm_vm/main.tf
@@ -24,6 +24,7 @@ resource "azurerm_network_interface" "vm_nic" {
     subnet_id                     = var.vnet_subnet_id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = var.create_public_ip ? azurerm_public_ip.vm_ip[0].id : null
+    primary                       = var.enable_ipv6 ? true : null
   }
 
   dynamic "ip_configuration" {
